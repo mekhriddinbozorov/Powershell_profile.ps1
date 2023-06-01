af1908a6eda191109e21f6e5ef686a9be8532ef2
Mehriddin <71074521+mekhriddinbozorov@users.noreply.github.com>
2023-06-01 17:27:00 +0500

    Update Powershell_profile.ps1

--- Powershell_profile.ps1
+++ Powershell_profile.ps1
@@ -1,1 +1,26 @@
 
+function prompt {
+    $p = Split-Path -Leaf -Path (Get-Location)
+    Write-Host " $p" -ForegroundColor DarkBlue -NoNewline
+
+    $branchName = $(git rev-parse --abbrev-ref HEAD 2>$null)
+    if ($branchName) {
+        Write-Host " (" -ForegroundColor DarkBlue -NoNewline
+
+        Write-Host "$branchName" -ForegroundColor DarkGreen -NoNewline
+
+        $untracked = $(git diff --stat | Measure-Object | Select-Object -ExpandProperty Count 2>$null)
+        if($untracked){
+            Write-Host " $($untracked-1)" -ForegroundColor DarkGray -NoNewline
+        }
+
+        $staged = $(git diff --stat --cached | Measure-Object | Select-Object -ExpandProperty Count 2>$null)
+        if($staged){
+            Write-Host " $($staged-1)" -ForegroundColor Yellow -NoNewline
+        }
+
+        Write-Host ")" -ForegroundColor DarkBlue -NoNewline
+    }
+    return " "
+}
+clear
